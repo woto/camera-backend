8e8c7d9f7f169d0705c2dd2c0e36d5284f2b94b4
Ruslan Kornev <oganer@gmail.com>
2025-12-28 14:05:05 +0100

    Fix js channels

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,9 +10,4 @@ pin_all_from "app/javascript/channels", under: "channels"
 pin "channels/consumer", to: "channels/consumer.js"
 pin "channels/ws_connections_channel", to: "channels/ws_connections_channel.js"
 pin "controllers/video_switcher_controller.js", to: "controllers/video_switcher_controller.js"
-pin "video.js" # @8.21.0
 pin "hls.js", to: "https://cdn.jsdelivr.net/npm/hls.js@1.5.0/dist/hls.min.js"
-pin "@babel/runtime/helpers/extends", to: "@babel--runtime--helpers--extends.js" # @7.28.4
-pin "@videojs/vhs-utils/es/byte-helpers", to: "@videojs--vhs-utils--es--byte-helpers.js" # @4.1.1
-pin "@videojs/vhs-utils/es/containers", to: "@videojs--vhs-utils--es--containers.js" # @4.1.1
-pin "@videojs/vhs-utils/es/decode-b64-to-uint8-array", to: "@videojs--vhs-utils--es--decode-b64-to-uint8-array.js" # @4.1.1
