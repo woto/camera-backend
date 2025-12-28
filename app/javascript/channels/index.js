73ec630bfe122065bcc4d56f737093a8b85ecd6c
Ruslan Kornev <oganer@gmail.com>
2025-12-28 13:27:02 +0100

    Fix js channels

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,4 +1,2 @@
-// Load all the channels within this directory and all subdirectories.
-// Channel files must be named *_channel.js.
-
-const channels = import.meta.glob('./*_channel.js', { eager: true })
+import "channels/consumer"
+import "channels/ws_connections_channel"
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,8 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/channels", under: "channels"
+pin "channels/consumer", to: "channels/consumer.js"
+pin "channels/ws_connections_channel", to: "channels/ws_connections_channel.js"
 pin "controllers/video_switcher_controller.js", to: "controllers/video_switcher_controller.js"
 pin "video.js" # @8.21.0
 pin "hls.js", to: "https://cdn.jsdelivr.net/npm/hls.js@1.5.0/dist/hls.min.js"
