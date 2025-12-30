28e4ff863bc21fdd39d0ecf497d701202ad72775
Ruslan Kornev <oganer@gmail.com>
2025-12-30 12:05:40 +0100

    Avoid FK problem on destroy

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,4 +10,15 @@ class Event < ApplicationRecord
 
   validates :captured_at, presence: true
   validates :captured_at, uniqueness: { scope: :room_id }
+
+  before_destroy :clear_capture_references, prepend: true
+
+  private
+
+  def clear_capture_references
+    capture_ids = captures.select(:id)
+    Capture.where(offset_base_capture_id: capture_ids).update_all(offset_base_capture_id: nil)
+    Event.where(base_capture_id: capture_ids).update_all(base_capture_id: nil)
+    update_columns(base_capture_id: nil)
+  end
 end
