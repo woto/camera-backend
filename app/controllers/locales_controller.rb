d3cb8493e8a2212c3e590b1ec225693946f5852c
Ruslan Kornev <oganer@gmail.com>
2026-01-06 00:59:32 +0100

    Refactor locale redirection logic to improve handling of referer URLs

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -6,6 +6,20 @@ class LocalesController < ApplicationController
     if I18n.available_locales.map(&:to_s).include?(locale)
       cookies[:locale] = { value: locale, expires: 1.year.from_now }
     end
-    redirect_back fallback_location: events_path
+    redirect_to locale_redirect_target, allow_other_host: false
+  end
+
+  def locale_redirect_target
+    fallback = events_path
+    referer = request.referer
+    return fallback if referer.blank?
+
+    uri = URI.parse(referer)
+    query = uri.query.present? ? Rack::Utils.parse_nested_query(uri.query) : {}
+    query.delete("locale")
+    uri.query = query.present? ? query.to_query : nil
+    uri.to_s
+  rescue URI::InvalidURIError
+    fallback
   end
 end
